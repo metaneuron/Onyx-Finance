daef60cc08ea4afa819a22b483d72a36c3aa4fdd
Kevin Wu <wu2 kevin@dny-c02xj1w3j.bcg.com>
2019-02-23 11:16:17 -0500

    Add User entity and UserDto

---
+++ backend/UserService/Dtos/UserDto.cs
@@ -0,0 +1,21 @@
+/*
+User DTO is a data transfer object used to send selected user data to and from
+the user's API endpoints. This DTO doesn't contain the PasswordHash
+and PasswordSalt fields of the user entity class, so not included in responses from 
+the web api when the controller maps data from user entities to user dtos.
+ 
+Password property in the DTO is only used for model binding data coming into the
+controller from HTTP requests (e.g. authenticate, register, etc.) passwords are 
+never included in responses from the web api. 
+*/
+
+namespace WebApi.Dtos
+{
+    public class UserDto {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ backend/UserService/Entities/User.cs
@@ -0,0 +1,17 @@
+/*
+The user entity class represents the data stored in the database for users.
+It's used by the Entity Framework Core to map relational data from the DB
+into .NET objects for data management and CRUD operations.
+*/
+
+namespace WebApi.Entities
+{
+    public class User {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string username { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
