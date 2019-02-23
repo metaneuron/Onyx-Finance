3ea0d6afea75bbcbd485c313655439f464a4170a
Kevin Wu <wu2 kevin@dny-c02xj1w3j.bcg.com>
2019-02-23 11:59:33 -0500

    Add Register and CRUD user endpoints

--- backend/UserService/Controllers/UsersController.cs
+++ backend/UserService/Controllers/UsersController.cs
@@ -77,4 +77,53 @@ namespace WebApi.Controllers {
             Token = tokenString
         });
     }
+
+    [AllowAnonymous]
+    [HttpPost("register")]
+    public IActionResult Register([FromBody]UserDto userDto) {
+        // map dto to entity
+        var user = _mapper.Map<User>(userDto);
+        try {
+            // saving user
+            _userService.Create(user, userDto.Password);
+            return Ok();
+        } catch (AppException ex) {
+            return BadRequest(new { message = ex.Message });
+        }
+    }
+
+    [HttpGet]
+    public IActionResult GetAll() {
+        var users = _userService.GetAll();
+        var userDtos = _mapper.Map<IList<userDto>>(users);
+        return Ok(userDtos);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id) {
+        var user = _userService.GetById(id);
+        var userDto = _mapper.Map<UserDto>(user);
+        return Ok(userDto);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult Update(int id, [FromBody]UserDto userDto) {
+        // map dto to entity and set id
+        var user = _mapper.Map<User>(userDto);
+        user.Id = id;
+
+        try {
+            // save
+            _userService.Update(user, userDto.Password);
+            return Ok();
+        } catch(AppException ex) {
+            return BadRequest(new { message = ex.Message });
+        }
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id) {
+        _userService.Delete(id);
+        return Ok();
+    }
 }
